6326f7e86b84166643b7f2834507d538eb6a30d2
Jose Ordiales <jose.ordiales@klarna.com>
2017-12-14 17:32:31 -0300

    Day 14

---
+++ src/test/groovy/me/jlordiales/aoc/Day14/Day14Test.groovy
@@ -0,0 +1,43 @@
+package me.jlordiales.aoc.Day14
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class Day14Test extends Specification {
+
+    @Unroll
+    def "squares used for #input == #squaresUsed"() {
+        expect:
+        Day14.squaresUsed(input) == squaresUsed
+
+        where:
+        input      | squaresUsed
+        "flqrgnkx" | 8108
+        "oundnydw" | 8106
+    }
+
+    @Unroll
+    def "hex to binary #hex == #binary"() {
+        expect:
+        Day14.hexToBin(hex) == binary
+
+        where:
+        hex   | binary
+        "f"   | "1111"
+        "0"   | "0000"
+        "010" | "000000010000"
+
+    }
+
+    @Unroll
+    def "regions for #input"() {
+        expect:
+        Day14.regions(input) == numberOfRegions
+
+        where:
+        input      | numberOfRegions
+        "flqrgnkx" | 1242
+        "oundnydw" | 1164
+    }
+}
