a60081cdef619fcbc0639665d1fc9c26571f6946
Jose Ordiales <jose.ordiales@klarna.com>
2017-12-17 01:07:58 -0300

    Refactor

--- src/main/groovy/me/jlordiales/aoc2017/Day16/Day16.groovy
+++ src/main/groovy/me/jlordiales/aoc2017/Day16/Day16.groovy
@@ -1,34 +1,32 @@
 package me.jlordiales.aoc2017.Day16
 
-import com.codepoetics.protonpack.StreamUtils
-
 import java.util.stream.Stream
+
+import static com.codepoetics.protonpack.StreamUtils.takeUntil
+import static java.util.stream.Collectors.toList
 
 class Day16 {
     public static final List<Character> INITIAL_POSITIONS = ('a'..'p').collect { it.toCharacter() }
 
-    static def dance(String input) {
-        danceNTimes(input, 1)
+    static String dance(String input) {
+        processInput(INITIAL_POSITIONS, input).join()
     }
 
     static def danceNTimes(String input, int n) {
-        positionsStream(input).skip(n)
-                              .findFirst()
-                              .get()
-                              .join()
+        def fullCycle = [INITIAL_POSITIONS] + danceUntilBackToStartingPosition(input).collect(toList())
+        fullCycle.get(n % fullCycle.size()).join()
+    }
+
+    private static Stream<List<Character>> danceUntilBackToStartingPosition(String input) {
+        def allPositions = positionsStream(input)
+        takeUntil(allPositions.skip(1)) { it == INITIAL_POSITIONS }
     }
 
     private static Stream<List<Character>> positionsStream(String input) {
-        Stream.iterate(INITIAL_POSITIONS) { dance(it, input) }
+        Stream.iterate(INITIAL_POSITIONS) { processInput(it, input) }
     }
 
-    static def cycleSize(String input) {
-        def cycleStream = StreamUtils.takeUntilInclusive(positionsStream(input).skip(1), { it == INITIAL_POSITIONS })
-
-        cycleStream.count()
-    }
-
-    static def dance(List<Character> programs, String input) {
+    private static List<Character> processInput(List<Character> programs, String input) {
         input.tokenize(',').each {
             programs = movePrograms(it, programs)
         }
@@ -36,7 +34,7 @@ class Day16 {
         programs
     }
 
-    static List<Character> movePrograms(String move, List<Character> positions) {
+    private static List<Character> movePrograms(String move, List<Character> positions) {
         if (move.startsWith("s")) {
             return moveXProgramsFromEndToFront(Integer.valueOf(move.substring(1)), positions)
         } else if (move.startsWith("x")) {
@@ -48,11 +46,11 @@ class Day16 {
         }
     }
 
-    static List<Character> swapProgramNames(String nameA, String nameB, List<Character> characters) {
+    private static List<Character> swapProgramNames(String nameA, String nameB, List<Character> characters) {
         swapPositions(characters.findIndexOf { it.toString() == nameA }, characters.findIndexOf { it.toString() == nameB }, characters)
     }
 
-    static List<Character> swapPositions(int positionA, int positionB, List<Character> characters) {
+    private static List<Character> swapPositions(int positionA, int positionB, List<Character> characters) {
         def swappedList = new ArrayList(characters)
         def positionBItem = characters[positionB]
         swappedList[positionB] = swappedList[positionA]
@@ -60,7 +58,7 @@ class Day16 {
         swappedList
     }
 
-    static List<Character> moveXProgramsFromEndToFront(int x, List<Character> positions) {
+    private static List<Character> moveXProgramsFromEndToFront(int x, List<Character> positions) {
         int dropQuantity = positions.size() - x
 
         def front = positions.take(dropQuantity)
--- src/test/groovy/me/jlordiales/aoc2017/Day16/Day16Test.groovy
+++ src/test/groovy/me/jlordiales/aoc2017/Day16/Day16Test.groovy
@@ -7,16 +7,11 @@ class Day16Test extends Specification {
 
     def "test"() {
         expect:
-        Day16.dance(['a', 'b', 'c', 'd', 'e'], "s1,x3/4,pe/b").join() == "baedc"
         Day16.dance(Input.forDay(16)) == "kbednhopmfcjilag"
     }
 
     def "test2"() {
-        given:
-        def input = Input.forDay(16)
-        int cycleSize = Day16.cycleSize(input)
-
         expect:
-        Day16.danceNTimes(input, 1000000000 % cycleSize) == "fbmcgdnjakpioelh"
+        Day16.danceNTimes(Input.forDay(16), 1000000000) == "fbmcgdnjakpioelh"
     }
 }
